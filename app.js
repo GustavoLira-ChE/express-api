35d0bbc09ee35afe195ff9170864d70d9cc158e7
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-26 16:38:50 -0500

    add: GET entpoint /v1/explorers

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ import express from "express";
 
 const app = express();
 
-//Use JSON
-app.use(express.json());
 //localhost port 3000
 const port = 3000;
+
+//Use JSON in the server
+app.use(express.json());
+
+//Routes
+//Methods: GET, POST, PUT, DELETE
+//create an entpoint to return a list
+app.get('/v1/explorers', (req, res) => {
+    console.log(`GET Explorers V1 API ${new Date()}`)
+    const explorer1 = { id: 1, name: 'explorer1'}
+    const explorer2 = { id: 2, name: 'explorer2'}
+    const explorers = [explorer1, explorer2]
+    // HTTP CODE STATUS: 200
+    res.status(200).json(explorers)
+});
+
 
 //Initialize the app
 app.listen(port, () => {
